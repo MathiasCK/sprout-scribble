8a52bab1a880b9fceefa8d8c81f86fc2bb7175f5
Mathias Kolberg <Mathias.ck2000@gmail.com>
2024-05-27 13:43:09 +0200

    Cleanup project

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,5 @@
-import { createPost, getPosts } from "~/server/actions";
-
-const Home = async () => {
-  const { success: posts, error } = await getPosts();
-  console.log(posts);
-
-  if (error) {
-    return <div>{error}</div>;
-  }
-
-  return (
-    <div>
-      {posts!.map(post => (
-        <h1 key={post.id}>{post.title}</h1>
-      ))}
-      <form action={createPost}>
-        <input type="text" name="title" placeholder="Post title" />
-        <button type="submit">Create post</button>
-      </form>
-    </div>
-  );
+const Home = () => {
+  return <div>Home</div>;
 };
 
 export default Home;
