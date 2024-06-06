1fc05c57dfbc585acb8de7d466db0ca0093a53da
Mathias Kolberg <Mathias.ck2000@gmail.com>
2024-06-06 09:07:50 +0200

    Reference user id on two factor token table

--- server/schema/tokens-table.ts
+++ server/schema/tokens-table.ts
@@ -1,5 +1,6 @@
 import { timestamp, pgTable, text, primaryKey } from "drizzle-orm/pg-core";
 import { createId } from "@paralleldrive/cuid2";
+import { users } from "~/server/schema";
 
 export const emailTokens = pgTable(
   "email_tokens",
@@ -40,6 +41,7 @@ export const twoFactorTokens = pgTable(
     token: text("token").notNull(),
     expires: timestamp("expires", { mode: "date" }).notNull(),
     email: text("email").notNull(),
+    userId: text("userId").references(() => users.id, { onDelete: "cascade" }),
   },
   vt => ({
     compoundKey: primaryKey({ columns: [vt.id, vt.token] }),
