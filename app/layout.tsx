2fd024376ed6488aa2e1aff0ed0bc0f5af27c6dd
Mathias Kolberg <Mathias.ck2000@gmail.com>
2024-06-06 06:59:06 +0200

    Handle theme toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "~/components/nav";
 import { cn } from "~/lib/utils";
+import { ThemeProvider } from "~/components/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,10 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={cn(inter.className, "px-6 md:px-12 max-w-7xl mx-auto")}>
-        <Navbar />
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/nav/user-button.tsx
+++ components/nav/user-button.tsx
@@ -10,10 +10,17 @@ import {
   DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback } from "~/components/ui/avatar";
+import { Switch } from "~/components/ui/switch";
+
 import Image from "next/image";
 import { LogOutIcon, Moon, SettingsIcon, Sun, TruckIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useState } from "react";
 
 const UserButton = ({ user }: Session) => {
+  const { theme, setTheme } = useTheme();
+  const [checked, setChecked] = useState<boolean>(false);
+
   if (!user) {
     return null;
   }
@@ -50,32 +57,54 @@ const UserButton = ({ user }: Session) => {
           </span>
         </div>
         <DropdownMenuSeparator />
-        <DropdownMenuItem className="group py-2 font-medium cursor-pointer transition-all duration-500">
+        <DropdownMenuItem className="group py-2 font-medium cursor-pointer">
           <TruckIcon
             size={14}
             className="mr-2 group-hover:translate-x-1 transition-all duration-300 ease-in-out"
           />
           My orders
         </DropdownMenuItem>
-        <DropdownMenuItem className="group py-2 font-medium cursor-pointer transition-all duration-500">
+        <DropdownMenuItem className="group py-2 font-medium cursor-pointer">
           <SettingsIcon
             size={14}
             className="mr-2 group-hover:rotate-180 transition-all duration-300 ease-in-out"
           />
           Settings
         </DropdownMenuItem>
-        <DropdownMenuItem className="py-2 font-medium cursor-pointer transition-all duration-500">
-          <div className="flex items-center">
-            <Sun size={14} />
-            <Moon size={14} />
-            <p>
-              Theme <span>theme</span>
-            </p>
-          </div>
-        </DropdownMenuItem>
+        {theme && (
+          <DropdownMenuItem className="py-2 font-medium cursor-pointer">
+            <div
+              className="flex items-center group"
+              onClick={e => e.stopPropagation()}
+            >
+              <div className="relative flex mr-2">
+                <Sun
+                  className="group-hover:text-yellow-600 absolute group-hover:rotate-180 dark:scale-0 dark:-rotate-90 transition-all duration-500 ease-in-out"
+                  size={14}
+                />
+                <Moon
+                  className="group-hover:text-blue-400 dark:scale-100 scale-0"
+                  size={14}
+                />
+              </div>
+              <p className="dark:text-blue-400 text-secondary-foreground/75 text-yellow-600">
+                {theme[0].toUpperCase() + theme.slice(1)} mode
+              </p>
+              <Switch
+                className="scale-75 ml-2"
+                checked={checked}
+                onCheckedChange={e => {
+                  setChecked(prev => !prev);
+                  setTheme(e ? "dark" : "light");
+                }}
+              />
+            </div>
+          </DropdownMenuItem>
+        )}
+
         <DropdownMenuItem
           onClick={() => signOut()}
-          className="group py-2 font-medium cursor-pointer transition-all duration-500"
+          className="group py-2 font-medium cursor-pointer"
         >
           <LogOutIcon
             size={14}
